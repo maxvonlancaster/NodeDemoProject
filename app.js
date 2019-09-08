147217b79efee5f99385555df177c7fb794fb695
windchange7 <windchange7@gmail.com>
2019-09-08 22:59:40 +0300

    express basis added two

--- app.js
+++ app.js
@@ -6,32 +6,17 @@ const fs = require("fs");
 const EventEmitter = require("events");
 const http = require("http");
 
-// logger.log("message one");
-// var pathObj = path.parse(__filename);
-// // console.log(pathObj);
+const express = require("express");
 
-// //listener
-// logger.on("messageLogged", arg => {
-//   console.log("CALLED : " + arg.id);
-// });
-
-// console.log(os.freemem());
-// console.log(`Total memory: ${os.totalmem}`);
-
-// fs.readdir("./", function(err, files) {
-//   if (err) console.log("Error", err);
-//   else console.log("Result", files);
-// });
-
-//event
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello there");
-    res.end();
-  }
+const app = express();
+app.get("/", function(request, response) {
+  response.send("<h1>Главная страница</h1>");
+  logger.log("Accessed ");
 });
-
-// server.on("connection", socket => {
-//   console.log("new connection");
-// });
-server.listen(3000);
+app.get("/about", function(request, response) {
+  response.send("<h1>О сайте</h1>");
+});
+app.get("/contact", function(request, response) {
+  response.send("<h1>Контакты</h1>");
+});
+app.listen(3000);

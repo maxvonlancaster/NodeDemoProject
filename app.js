f2bbaca2cae83b6a85ec3bffbec475ec3a6d668d
windchange7 <windchange7@gmail.com>
2019-09-10 10:54:40 +0300

    express midddlewares added

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+app.use(function(request, response, next) {
+  console.log("Middleware 1");
+  next();
+});
+app.use("/about", function(request, response, next) {
+  console.log("Middleware 2");
+  next();
+});
 app.get("/", function(request, response) {
   response.send("<h1>Главная страница</h1>");
   logger.log("Accessed ");
@@ -16,7 +24,11 @@ app.get("/", function(request, response) {
 app.get("/about", function(request, response) {
   response.send("<h1>О сайте</h1>");
 });
-app.get("/contact", function(request, response) {
+app.get("/contact(.html)?", function(request, response) {
   response.send("<h1>Контакты</h1>");
+});
+app.get(/.*(\.)html$/, function(request, response) {
+  response.send();
+  console.log(request.ip);
 });
 app.listen(3000);

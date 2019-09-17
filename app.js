c042fd8d80e82514bcda1610786835c556c0cc1b
maxvonlancaster <38358502+maxvonlancaster@users.noreply.github.com>
2019-09-17 14:16:20 +0300

    express another one

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
 app.use(function(request, response, next) {
   console.log("Middleware 1");
   next();
@@ -17,18 +20,41 @@ app.use("/about", function(request, response, next) {
   console.log("Middleware 2");
   next();
 });
-app.get("/", function(request, response) {
-  response.send("<h1>Главная страница</h1>");
-  logger.log("Accessed ");
-});
 app.get("/about", function(request, response) {
   response.send("<h1>О сайте</h1>");
 });
+app.get("/main", function(request, response){
+  response.sendFile(__dirname + "/index.html");
+  logger.log("File send ")
+});
+app.get("/wrongpage", function(request, response){
+  response.sendStatus(404).send("Resource not found");
+  logger.log("404 accessed");
+});
+
+// Query string: adress should be: /requeststring?id=1&name=John
+app.get("/requeststring", function(request, response){
+  let id = request.query.id;
+  let name = request.query.name;
+  response.send("<h1>YOUR QUERY STRING</h1><p>id = " + id + "</p><p>name = " + name + "</p>");
+})
+
 app.get("/contact(.html)?", function(request, response) {
   response.send("<h1>Контакты</h1>");
 });
-app.get(/.*(\.)html$/, function(request, response) {
+app.get(/.*(\.)html$/, function(request, response) { // regex - route to all ending in htmls
   response.send();
   console.log(request.ip);
 });
+app.get("/", function(request, response) { // More general routes go after rarer
+  response.send("<h1>Главная страница</h1>");
+  logger.log("Accessed ");
+}); 
+
+// Redirect to https://docs.microsoft.com/en-us/dotnet/core/tools/cli-msbuild-architecture
+app.use("/redirect", function(request, response){
+  response.redirect("https://docs.microsoft.com/en-us/dotnet/core/tools/cli-msbuild-architecture");
+  logger.log("Redirected ");
+});
+
 app.listen(3000);
